63b94c0f81320274fcabdf178c6aab4a0b0dcdae
matamong <matamatamong@gmail.com>
2020-11-03 00:44:22 +0900

    Modify Navbar - Apply material UI icon, button - Apply navbar.scss

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,21 +2,44 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../actions/auth';
+import './navbar.scss';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const Navbar = ({ logout, isAuthenticated }) => {
     const guestLinks = () => (
-        <Fragment>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign Up</Link>
-        </Fragment>
+        <div className="header__profile">
+            <Link to="/login">
+                <IconButton>
+                    <PersonIcon className="header__icon" fontSize="large" />
+                </IconButton>
+            </Link>
+        </div>
     );
 
     const authLinks = () => (
-        <a href="#!" onClick={logout}>Logout</a>
+        <div className="header__profile">
+            <Link to="/profile">
+                <IconButton>
+                    <PersonIcon className="header__icon" fontSize="large" />
+                </IconButton>
+            </Link>
+            <IconButton>
+                <ExitToAppIcon className="header__icon" fontSize="large" onClick={logout} />
+            </IconButton>
+        </div>
     );
-    
+
     return (
-        <div>{isAuthenticated ? authLinks() : guestLinks()}</div>
+        <div className="header">
+            <Link to="/">
+                <img className="header__log" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                    alt="tinder logo"></img>
+            </Link>
+            {isAuthenticated ? authLinks() : guestLinks()}
+        </div>
     );
 };
 
