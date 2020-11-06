0478df9e1d2c5ba98b47cfc55ae2399a99ec123b
matamong <matamatamong@gmail.com>
2020-11-06 20:42:28 +0900

    <Test> : Test GameModel

---
+++ backend/matching/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from ..models import Game, UserGame
+from accounts.models import UserAccount
+
+class BaseModelTestCase(TestCase):
+
+    # 클래스 전체에서 사용되는 설정으로 테스트 할 때 딱 한 번만 실행된다.
+    @classmethod
+    def setUpClass(cls):
+        super(BaseModelTestCase, cls).setUpClass()
+        cls.user = UserAccount(
+            email = 'gameduosdev@gmail.com',
+            name = 'gameduosdev',
+            is_active = 1,
+            is_staff = 1,
+        )
+        cls.user.save()
+        cls.game = Game(
+            name = 'lol', 
+            genre = 'AOS', 
+            developer = 'Riot',
+            publisher = 'Riot'
+            )
+        cls.game.save()
+        cls.usergame = UserGame(
+            user = cls.user,
+            game = cls.game,
+            game_nickname = '마석용',
+            is_random_agreed = 1,
+            is_profile_agreed = 1,
+            self_introduction = 'ㅎㅇ'
+        )
+        cls.usergame.save()
+
+class GameModelTestCase(BaseModelTestCase):
+    def test_created_properly(self):
+        self.assertEqual(self.game.name, 'lol')
+        self.assertEqual(self.game.genre, 'AOS')
