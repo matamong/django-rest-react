ec8bda709427d730bca3ae47b66f143fd42e94c4
matamong <matamatamong@gmail.com>
2020-11-22 20:19:10 +0900

    <:art: feature> : Apply error alert in lolMatching reducer

--- frontend/src/actions/lolMatching.js
+++ frontend/src/actions/lolMatching.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import {
     LOL_USER_GAME_SAVE_SUCCESS,
     LOL_USER_GAME_SAVE_FAIL,
+    SET_ERROR_ALERT,
+    REMOVE_ALERT,
     LOL_USER_GAME_UPDATE_SUCCESS,
     LOL_USER_GAME_DELETE_SUCCESS
 } from './types'
+import { setAlert, setErrorAlert } from './alert';
+
 
 export const save_lol_usergame = ( lol_name, region, prefer_style, prefer_time, intro, lol_position, lol_prefer_mode ) => async dispatch => {
     const config = {
@@ -26,9 +30,18 @@ export const save_lol_usergame = ( lol_name, region, prefer_style, prefer_time, 
             type: LOL_USER_GAME_SAVE_SUCCESS,
             payload: res.data
         })
-    } catch (err) {
+    } catch (error) {
+        
+        console.log(error.response)
         dispatch({
-            type: LOL_USER_GAME_SAVE_FAIL
+            type: LOL_USER_GAME_SAVE_FAIL,
+            error: error
         })
+        if(error.response.status == 401)
+            dispatch(setErrorAlert('인증되지 않은 회원입니다!', 401))
+        else if(error.response.status == 400)
+            dispatch(setErrorAlert('작성하지 않은 곳이 있습니다!', 400))
+        else
+            dispatch(setErrorAlert('오류가 발생했습니다!', error.response.status));
     }
 }
