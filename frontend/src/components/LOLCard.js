c873da1592b6ced65943b9b84fa0cbbc0a57251a
matamong <matamatamong@gmail.com>
2020-11-27 20:25:12 +0900

    < :construction: feature > : Create LOLCard.js - Work in progress...

---
+++ frontend/src/components/LOLCard.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import './lolcard.scss';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+const LOLCard = (props) => {
+    
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {props.region}
+        </Typography>
+        <Typography variant="h5" component="h2">
+            {props.name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          자ㅏ기소개ㅐㅐ{props.intro}
+          <br />
+          빡즐겜수치: {props.prefer_style}
+          <br />
+          시간대ㅐㅐㅐ: {props.prefer_time}
+        </Typography>
+      </CardContent>
+    </Card>
+    )
+}
+
+export default LOLCard
