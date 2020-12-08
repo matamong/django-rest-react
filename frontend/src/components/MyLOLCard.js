743515b4ec856cfb2b4ccf78c1609639f9353eff
matamong <matamatamong@gmail.com>
2020-12-08 16:11:50 +0900

    <:art: Update> : Delete Redux state & Apply axios

--- frontend/src/components/MyLOLCard.js
+++ frontend/src/components/MyLOLCard.js
@@ -1,23 +1,49 @@
 import React, { useState, useEffect  } from 'react'
 import axios from 'axios'
-import { connect, useDispatch } from 'react-redux';
 import LOLCard from './LOLCard'
 
-// Loading 만들기
-// 등록하기 만들기
 
-// user id 가져오고 
-    // .then lolProfile 가져오고
-    // .then data dragon (img, 설명등등) 가져오기.
+const MyLOLCard = () => {
+    const [profile, setProfile] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-// Redux state에 의존하지말자. async라서 바로바로 못 쓰고 걍 잘 쓰지도 못할꺼,,,그냥 하나씩 데려와
-// https://stackoverflow.com/questions/61703393/react-useeffect-and-axios-making-chained-api-calls-within-then
-// https://stackoverflow.com/questions/57376297/how-to-make-a-second-api-call-based-on-the-first-response
+    const fetchProfile = async () => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + `${localStorage.getItem('access')}`,
+                'Accept': 'application/json'
+            }
+        }
+        try {
+            setError(null);
+            setProfile(null);
+            setLoading(true);
+            
+            axios.get(`${process.env.REACT_APP_API_URL}/api/matching/lol/my/usergame`, config)
+            .then(function (response) {
+                console.log(response);
+                setProfile(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        } catch (e) {
+            setError(e);
+            console.log(e)
+        }
+        setLoading(false)
+    }
 
-// 아니면 !!!
-// 그냥 action에서 dataDragon으로 main champ url처리를 해버려
-const MyLOLCard = ({ lolProfile }) => {
-    const profile = lolProfile
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
+    if (loading) return <div>Loading..</div>
+    if (error) return <div>Error!</div>
+    if (!profile) return null
+
 
     return (
         <div>
@@ -39,11 +65,5 @@ const MyLOLCard = ({ lolProfile }) => {
     )
 }
 
-const mapStateToProps = (state) => {
-    console.log(state)
-    return {
-        lolProfile: state.matching.lolProfile
-    }
-}
 
-export default connect(mapStateToProps)(MyLOLCard)
+export default MyLOLCard
