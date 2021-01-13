d4b445a533749012f05052d8646f62259e8d862d
matamong <matamatamong@gmail.com>
2021-01-13 19:49:13 +0900

    <:art: Update> : Mypage 컴포넌트에 axios 적용 - REST API 주소: /api/users/my 사용

--- frontend/src/containers/MyPage.js
+++ frontend/src/containers/MyPage.js
@@ -7,12 +7,52 @@ import './mypage.scss'
 
 
 const MyPage = () => {
+    const [profile, setProfile] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
 
+    const fetchProfile = async () => {
+        const config = {
+            headers : {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + `${localStorage.getItem('access')}`,
+                'Accept': 'application/json'
+            }
+        }
+        try {
+            setError(null);
+            setProfile(null);
+            setLoading(true);
+            
+            axios.get(`${process.env.REACT_APP_API_URL}/api/users/my`, config)
+            .then(function (response) {
+                console.log(response);
+                setProfile(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        } catch (e) {
+            setError(e);
+            console.log(e)
+        }
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
+    if (loading) return <div>Loading..</div>
+    if (error) return <div>Error!</div>
+    if (!profile) return null
+
+    console.log(profile)
     return (
         <div className="mypage__container">
             <div className="mypage__profileToken__container">
                 <div className="mypage__profileToken__item">
-                    <ProfileToken />
+                    <ProfileToken profile={profile} />
                 </div>
             </div>
             <div className="mypage__card__container">
