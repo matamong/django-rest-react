d6889753c889d330d0d18dd86cf6bae568d16849
matamong <matamatamong@gmail.com>
2021-01-14 19:53:37 +0900

    <:lipstick: style> : 프로필 상태메뉴 추가 및 반응형 적용

--- frontend/src/containers/MyPage.js
+++ frontend/src/containers/MyPage.js
@@ -4,7 +4,13 @@ import ProfileToken from '../components/ProfileToken'
 import axios from 'axios'
 import { connect } from 'react-redux';
 import './mypage.scss'
-
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 
 const MyPage = () => {
     const [profile, setProfile] = useState(null)
@@ -48,18 +54,44 @@ const MyPage = () => {
     if (!profile) return null
 
     console.log(profile)
+
+    
     return (
         <div className="mypage__container">
             <div className="mypage__profileToken__container">
+                <div className="mypage__profileToken__background"></div>
                 <div className="mypage__profileToken__item">
                     <ProfileToken profile={profile} />
                 </div>
             </div>
+            <div className="mypage__profilemenue__container">
+                <div className="mypage__profilemenue__item">
+                    <Paper square>
+                        <Tabs
+                            variant="fullWidth"
+                            indicatorColor="secondary"
+                            textColor="secondary"
+                            aria-label="icon label tabs example"
+                        >
+                            <Tab icon={<FavoriteIcon />} label="50" />
+                            <Tab icon={<ChatBubbleOutlineIcon />} label="50" />
+                            <Tab icon={<ThumbUpAltIcon />} label="34" />
+                        </Tabs>
+                            
+                    </Paper>
+                </div>
+            </div>
             <div className="mypage__card__container">
+                <h2 className="mypage__card__title">
+                    <div><SportsEsportsIcon /></div><div>등록된 매칭게임</div>
+                </h2>
                 <ul className="mypage__card__item">
                     <li><MyLoLCard /></li>
                 </ul>                
             </div>
+            <div>아바타바꾸기/호칭바꾸기 는 프로필 토큰에서 해부러,  
+                회원정보 수정비번수정/삭제, 
+                매칭 등록/수정/삭제는 각 카드에 해부러, </div>
         </div>
     )
 }
