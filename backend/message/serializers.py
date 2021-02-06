46ec37b02a36e73ad4cde7cf5b9d4bef2b510138
matamong <matamatamong@gmail.com>
2021-02-06 16:56:48 +0900

    <:art: update> : 메시지룸 serializer에 Better Serializing 적용 - 유저의 매칭요청게임, 수락여부에 따라 각각 다르게 게임프로필을 serializing하여 응답에 추가해줄 수 있게 해준다.

--- backend/message/serializers.py
+++ backend/message/serializers.py
@@ -1,18 +1,39 @@
 from rest_framework import serializers
 from .models import MessageRoom, Message
+from lolapi.models import LolUserGame
 from accounts.serializers import ProfileSerializer
+from .better_seralizer import BetterGameProfileSerializing
+
 
 
 class MessageRoomSerializer(serializers.ModelSerializer):
     receiver = ProfileSerializer(read_only=True)
     sender = ProfileSerializer(read_only=True)
+    sender_usergame_profile = serializers.SerializerMethodField()
+    receiver_usergame_profile = serializers.SerializerMethodField()
+    
+    better = BetterGameProfileSerializing()
 
     class Meta:
         model = MessageRoom
         fields = '__all__'
-        extra_kwargs = {'receiver': {'read_only':True}, 'sender': {'read_only':True}}
+        extra_kwargs = {
+            'sender': {'read_only':True}, 
+            'sender_usergame_profile': {'read_only': True},
+            'receiver': {'read_only':True},
+            'receiver_usergame_profile': {'read_only': True}
+        }
+    
+    def get_sender_usergame_profile(self, obj):
+        return self.better.get_seralized_game_profile(obj.sender, obj.game_name, obj.receiver_consent)
 
+    def get_receiver_usergame_profile(self, obj):
+        if obj.receiver_consent == True :
+            return self.better.get_seralized_game_profile(obj.receiver, obj.game_name, obj.receiver_consent)
+        else:
+            return {}
 
+        
 class MessageSerializer(serializers.ModelSerializer):
     reply_user = ProfileSerializer(read_only=True)
 
