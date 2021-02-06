8d347248b129003df33c7eeb05cc3ce0da0f5756
matamong <matamatamong@gmail.com>
2021-02-06 17:16:23 +0900

    <:art: update>: user field 추가 및 lol_name은 쓰기전용으로 변경 - lol 닉네임 노출을 막기위해 lol닉네임은 write_only 바꾸되, user 정보를 대신 응답

--- backend/lolapi/serializers.py
+++ backend/lolapi/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import LolUserGame, LolPosition, LolPreferMode
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from .datadragon import DataDragon
+from accounts.serializers import ProfileSerializer
+
 
 class LolPositionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,11 +23,17 @@ class LolUserGameSerializer(WritableNestedModelSerializer, serializers.ModelSeri
     lol_position = LolPositionSerializer()
     lol_prefer_mode = LolPreferModeSerializer()
     main_champ_info = serializers.SerializerMethodField()
+    user = ProfileSerializer(read_only=True)
 
     class Meta:
         model = LolUserGame
         fields = ['lol_name', 'region', 'prefer_style', 'prefer_time', 
-        'intro', 'lol_position', 'lol_prefer_mode', 'solo_tier' , 'solo_rank', 'main_champ_info', 'mic']
+        'intro', 'lol_position', 'lol_prefer_mode', 'solo_tier' , 'solo_rank', 'main_champ_info', 'mic', 'user']
+        extra_kwargs = {
+            'user': {'read_only': True},
+            'lol_name': {'write_only': True}
+        }
+
     
     def create(self, validated_data):
         lol_position_data = validated_data.pop('lol_position')
