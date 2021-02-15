c9e4e369aabd21c420fc4c1cc6e168d7eda1f75c
matamong <matamatamong@gmail.com>
2021-02-15 19:20:57 +0900

    <:art: update> : 채팅 디테일 메시지 보내는 기능 추가 및 자동 스크롤 기능 추가

--- frontend/src/components/ChatScreen.js
+++ frontend/src/components/ChatScreen.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { connect } from 'react-redux';
+import { send_matching_message } from '../actions/matching';
 import './chatscreen.scss'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom'
 import Avatar from '@material-ui/core/Avatar'
 
 
-const ChatScreen = ({props, isAuthenticated, user }) => {
+const ChatScreen = ({ props, isAuthenticated, user, send_matching_message }) => {
     const [chatContents, setChatContents] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
     const [input, setInput] = useState(''); //keep track input type
-  
+    
+    const messageRef  = useRef();
+    
     const pathname = useLocation().pathname;
     var regex = /\d+/g;
     var messageroom_id = Number(pathname.match(regex));
 
-    const fetchChatContent = async() => {
+    const fetchChatContent = async () => {
         const config = {
             headers: {
                 'Content-Type': 'application/json',
@@ -29,16 +32,16 @@ const ChatScreen = ({props, isAuthenticated, user }) => {
         try {
             setError(null);
             setChatContents(null);
-            setLoading(true);   
+            setLoading(true);
             console.log('으아아앙')
             axios.get(`${process.env.REACT_APP_API_URL}/api/messages/` + messageroom_id, config)
-            .then(function (response) {
-                console.log(response.data);
-                setChatContents(response.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+                .then(function (response) {
+                    console.log(response.data);
+                    setChatContents(response.data)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         } catch (e) {
             setError(false);
             console.log(e)
@@ -47,48 +50,57 @@ const ChatScreen = ({props, isAuthenticated, user }) => {
     }
 
     useEffect(() => {
-        fetchChatContent();
+        fetchChatContent()
     }, []);
 
+    useEffect(() => {
+        if (messageRef.current) {
+            messageRef.current.scrollIntoView(
+              {
+                behavior: 'smooth',
+                block: 'end',
+                inline: 'nearest'
+              })
+          }
+    },[chatContents])
+
     const handleSend = (e) => {
-        e.preventDefault(); //엔터칠때 리프레쉬 되는거 방지해줌
-        //setChatContents([...chatContents, { input }]);
-        setInput(""); //Input 초기화
+        send_matching_message(messageroom_id, input).then(setInput(""))
     };
 
     if (loading) return <div>loading..</div>
     if (error) return <div>error!</div>
     if (!chatContents) return null
 
-
+    
     return (
         <div>
-            <div className="chatScreen">
-            <p className="chatScreen__timestamp">You matched with matamong on 10/08/20</p>
-            {chatContents.map((message) => 
-                message.reply_user.name !== user.name ? (
-                    <div className="chatScreen__message">
-                        <Avatar
-                            calssName="chatScreen__image"
-                        />
-                        <p className="chatScreen__text">{message.content}</p>
-                    </div>
-                ) : (
-                        <div className="chatScreen__message">
-                            <p className="chatScreen__textUser">{message.content}</p>
+            <div className="chatScreen" ref={messageRef}>
+                <p className="chatScreen__timestamp">You matched with matamong on 10/08/20</p>
+                {chatContents.map((message, index) =>
+                    message.reply_user.name !== user.name ? (
+                        <div className="chatScreen__message" key={index} id={index}>
+                            <Avatar
+                                calssName="chatScreen__image"
+                            />
+                            <p className="chatScreen__text">{message.content}</p>
                         </div>
-                )
-            )}
-            <form className="chatScreen__input">
-                <input
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                    className="chatScreen__inputField"
-                    placeholder="메시지를 입력하세요."
-                    type="text" />
-                <button onClick={handleSend} type="submit" className="chatScreen__inputButton">보내기</button>
-            </form>
-        </div>
+                    ) : (
+                            <div className="chatScreen__message" key={index} id={index}>
+                                <p className="chatScreen__textUser">{message.content}</p>
+                            </div>
+                        )
+                )}
+                <form className="chatScreen__input">
+                    <input
+                        value={input}
+                        onChange={e => setInput(e.target.value)}
+                        className="chatScreen__inputField"
+                        placeholder="메시지를 입력하세요."
+                        type="text" />
+                    <button onClick={handleSend} type="submit" className="chatScreen__inputButton">보내기</button>
+                </form>
+            </div>
         </div>
     )
 }
@@ -98,4 +110,4 @@ const mapStateToProps = state => ({
     user: state.auth.user
 });
 
-export default connect(mapStateToProps)(ChatScreen)
+export default connect(mapStateToProps, { send_matching_message })(ChatScreen)
