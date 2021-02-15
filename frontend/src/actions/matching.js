a59068d0788ecf46fd111a8bffd32c1728780c0c
matamong <matamatamong@gmail.com>
2021-02-15 17:35:27 +0900

    <:sparkles: feature> : 롤 매칭 메시지 보내는 모달 추가 및 csrf쿠키 적용 - 옆으로 스와이프하면 메시지를 보내는 모달 창이 나올 수 있게 구현 - 챗룸을 만들고 메시지를 보내는 두 가지 일을 수행

--- frontend/src/actions/matching.js
+++ frontend/src/actions/matching.js
@@ -13,7 +13,26 @@ import {
 import { setAlert, setErrorAlert } from './alert';
 import { load_user } from '../actions/auth';
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
+var axiosDefaults = require("axios/lib/defaults");
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+var csrftoken = getCookie('csrftoken');
 
 export const save_lol_usergame = ( lol_name, region, prefer_style, prefer_time, intro, lol_position, lol_prefer_mode, mic ) => async dispatch => {
     const config = {
@@ -149,3 +168,61 @@ export const load_lol_usergame = () => async dispatch => {
         });
     }
 }
+
+export const create_matching_message_room = ( receiver, game_name ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + `${localStorage.getItem('access')}`,
+            'Accept': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify( { receiver, game_name })
+    console.log(body)
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/messages/rooms`, body, config)
+
+        dispatch(setAlert('매칭이 요청되었어요!'))
+
+        return res.data
+    } catch (error) {
+        if(error.response.status == 401)
+            dispatch(setErrorAlert('인증되지 않은 회원입니다!', 401))
+        else if(error.response.status == 400)
+            dispatch(setErrorAlert('이미 존재하는 매칭방이에요!', 400))
+        else
+            dispatch(setErrorAlert('오류가 발생했어요!', error.response.status));
+    }
+}
+
+export const send_matching_message = ( message_room_id, content ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + `${localStorage.getItem('access')}`,
+            'Accept': 'application/json',
+            'X-CSRFToken': csrftoken
+        }
+    }
+
+    console.log(csrftoken)
+
+    const body = JSON.stringify( { content })
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/messages/`+message_room_id , body, config)
+        dispatch(setAlert('메시지가 보내졌어요.'))
+        return true
+    } catch (error) {
+        if(error.response.status == 401)
+            dispatch(setErrorAlert('인증되지 않은 회원입니다!', 401))
+        else if(error.response.status == 400)
+            dispatch(setErrorAlert('한마디를 작성해주세요!', 400))
+        else
+            dispatch(setErrorAlert('오류가 발생했어요!', error.response.status));
+            return false
+    }
+}
+
