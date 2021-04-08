c9d037183e25c66df57396693abf5a490961dd3a
matamong <matamatamong@gmail.com>
2021-04-08 17:02:34 +0900

    <:sparkles: feature> : 유저 정보에 Profile 모델 추가및 REST API GET/POST 생성 - Avatar, Title 모델 생성 및 이를 외부키로 한 Profile모델 생성 - 유저의 아바타(이름, 이미지주소)와 칭호를 담당 - 유저의 Profile이 없으면 Serializer에서 기본 아바타, 칭호를 Return한다.

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserAccount
+from .models import UserAccount, Profile, Avatar, Title
 
 # Register your models here.
 
@@ -7,4 +7,16 @@ class UserAdmin(admin.ModelAdmin):
     list_diplay = ('email', 'name')
     search_fields = ('email', 'name')
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar', 'title')
+
+class AvatarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 admin.site.register(UserAccount, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Avatar, AvatarAdmin)
+admin.site.register(Title, TitleAdmin)
