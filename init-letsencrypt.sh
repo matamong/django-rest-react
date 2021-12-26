405aa9f4e6ef4664f1a5a70e8ce03ec9b2e2ccbf
Ubuntu <ubuntu@ip-172-31-15-34.ap-northeast-2.compute.internal>
2021-12-26 11:19:32 +0000

    <:ambulance: bug> : Change letsencrypt setting for release

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(gameduos.net www.gameduos.net)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="gameduosdev@gmail.com" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
